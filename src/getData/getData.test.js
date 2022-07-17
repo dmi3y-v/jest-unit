611d3824555909f3a615cacad6197084779f9217
dmi3y-v <dmi3y.vagin@yandex.ru>
2022-07-17 15:27:28 +0300

    finish

---
+++ src/getData/getData.test.js
@@ -0,0 +1,67 @@
+const getData = require("./getData");
+const axios = require("axios");
+
+jest.mock("axios");
+
+describe("getData", () => {
+  let response;
+  beforeEach(() => {
+    response = {
+      data: [
+        {
+          id: 1,
+          name: "Clementine Bauch",
+          username: "Samantha",
+          email: "Nathan@yesenia.net",
+          address: {
+            street: "Douglas Extension",
+            suite: "Suite 847",
+            city: "McKenziehaven",
+            zipcode: "59590-4157",
+            geo: {
+              lat: "-68.6102",
+              lng: "-47.0653",
+            },
+          },
+          phone: "1-463-123-4447",
+          website: "ramiro.info",
+          company: {
+            name: "Romaguera-Jacobson",
+            catchPhrase: "Face to face bifurcated interface",
+            bs: "e-enable strategic applications",
+          },
+        },
+        {
+          id: 2,
+          name: "Patricia Lebsack",
+          username: "Karianne",
+          email: "Julianne.OConner@kory.org",
+          address: {
+            street: "Hoeger Mall",
+            suite: "Apt. 692",
+            city: "South Elvis",
+            zipcode: "53919-4257",
+            geo: {
+              lat: "29.4572",
+              lng: "-164.2990",
+            },
+          },
+          phone: "493-170-9623 x156",
+          website: "kale.biz",
+          company: {
+            name: "Robel-Corkery",
+            catchPhrase: "Multi-tiered zero tolerance productivity",
+            bs: "transition cutting-edge web services",
+          },
+        },
+      ],
+    };
+  });
+  test("Корректное значение", async () => {
+    axios.get.mockReturnValue(response);
+    const data = await getData();
+    expect(axios.get).toBeCalledTimes(1);
+    //expect(data).toEqual(["1", "2"]);
+    expect(data).toMatchSnapshot();
+  });
+});
--- src/square/square.test.js
+++ src/square/square.test.js
@@ -1,21 +1,18 @@
 const square = require("./square");
 
 describe("square", () => {
-  let number = 0;
-  let expectedResult = 0;
-
-  beforeEach(() => {
-    number = 10;
-    expectedResult = number * number;
-  });
-
-  test(`${number}^2 = ${expectedResult}`, () => {
-    expect(square(number)).toBe(expectedResult);
-  });
+  // let number = 0;
+  // let expectedResult = 0;
+  // beforeEach(() => {
+  //   number = 10;
+  //   expectedResult = number * number;
+  // });
   // test(`${number}^2 = ${expectedResult}`, () => {
   //   expect(square(number)).toBe(expectedResult);
   // });
-
+  // test(`${number}^2 = ${expectedResult}`, () => {
+  //   expect(square(number)).toBe(expectedResult);
+  // });
   // test("spy for 2", () => {
   //   const spyMathPow = jest.spyOn(Math, "pow");
   //   square(2);
@@ -26,7 +23,7 @@ describe("square", () => {
   //   square(1);
   //   expect(spyMathPow).toBeCalledTimes(0);
   // });
-
   // afterEach(() => {
   //   jest.clearAllMocks();
-  /
+  // });
+});
